e37deccfc309a58908638586d8d1a0904a36b896
pedro.korb <pedro.korb@irriga.net>
2021-08-11 11:33:10 -0300

    adicionando eslint e editorconfig

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react'
 
 function App() {
   return (
-    <div className="flex justify-center">
+    <div 
+    className="flex justify-center">
       <h1 className="bg-purple-900 text-white text-xl font-bold p-10 rounded-full mt-32">Tech Hour Front-end</h1>
     </div>
   )
