5551571b4a8aa2065f32e9fe75a35f5397d9947a
pedro.korb <pedro.korb@irriga.net>
2021-08-11 10:54:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center">
+      <h1 className="bg-purple-900 text-white text-xl font-bold p-10 rounded-full mt-32">Tech Hour Front-end</h1>
     </div>
-  );
+  )
 }
 
 export default App;
